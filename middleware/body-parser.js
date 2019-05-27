14c70420c3b3f2a4512db8a288738c4b2b1d3a52
Jonathan Martin <me@nybblr.com>
2019-05-27 09:20:47 -0400

    Funky spacing.

--- middleware/body-parser.js
+++ middleware/body-parser.js
@@ -3,7 +3,7 @@ const streamToPromise = require('../lib/stream-to-promise');
 let streamToJSONPromise = async stream => JSON.parse(await streamToPromise(stream));
 
 let bodyParser = {
-  json: () => (ctx, next) =>next({ ...ctx,body: streamToJSONPromise(ctx.req) }),
+  json: () => (ctx, next) => next({ ...ctx, body: streamToJSONPromise(ctx.req) }),
 };
 
 module.exports = bodyParser;
